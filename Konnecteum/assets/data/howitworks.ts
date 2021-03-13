e836d89ecb7a2f98f65339a5f6ac509c0812618c
tush1995 <tushar@kryptominds.com>
2021-03-13 22:07:40 +0530

    commit

---
+++ Konnecteum/assets/data/howitworks.ts
@@ -0,0 +1,67 @@
+export const HOWITWORK_DETAILS_ONE = [
+    {
+        "id": 1,
+        "hiw": "Post Project",
+        "imageUrl": "assets/img/color-icon/post-project.png",
+        "description": "Lorem ipsum dolor sit amet,consectetur adipiscing elit.",
+    },
+    {
+        "id": 2,
+        "hiw": "Recommendation",
+        "imageUrl": "assets/img/color-icon/recommendations.png",
+        "description": "Lorem ipsum dolor sit amet,consectetur adipiscing elit.",
+    },
+    {
+        "id": 3,
+        "hiw": "Live Chat",
+        "imageUrl": "assets/img/color-icon/live-chat.png",
+        "description": "Lorem ipsum dolor sit amet,consectetur adipiscing elit.",
+    },
+    {
+        "id": 4,
+        "hiw": "Review, Approve & Pay",
+        "imageUrl": "assets/img/color-icon/review-and-approve.png",
+        "description": "Lorem ipsum dolor sit amet,consectetur adipiscing elit.",
+    },
+    {
+        "id": 5,
+        "hiw": "Review & Ratings",
+        "imageUrl": "assets/img/color-icon/review-and-ratings.png",
+        "description": "Lorem ipsum dolor sit amet,consectetur adipiscing elit.",
+    }
+]
+
+
+export const CATEGORY_DETAILS = [
+    {
+        "id": 1,
+        "hiw": "Post Project",
+        "imageUrl": "assets/img/color-icon/post-project.png",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit,",
+    },
+    {
+        "id": 2,
+        "hiw": "Recommendation ",
+        "imageUrl": "assets/img/color-icon/recommendations.png",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
+    {
+        "id": 3,
+        "hiw": "Live Chat",
+        "imageUrl": "assets/img/color-icon/live-chat.png",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+
+    },
+    {
+        "id": 4,
+        "hiw": "Review, Approve & Pay",
+        "imageUrl": "assets/img/color-icon/review-and-approve.png",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
+    {
+        "id": 5,
+        "hiw": "Review & Ratings",
+        "imageUrl": "assets/img/color-icon/review-and-ratings.png",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    }
+]
